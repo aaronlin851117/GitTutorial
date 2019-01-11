3331a489eb1b5cf4ab943587e85be6caa209da7e
林劭謙 <aaronlin@linshaoqians-macbook-pro.local>
2019-01-11 21:17:24 +0800

    Add a reverse label

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var reverseLabel: UILabel!
+    
     let message = "Hello Git!"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
-        print(reverse(text: "Stressed"))
+        
+        let reversed = reverse(text: "Stressed")
+        print(reversed)
+        reverseLabel.text = reversed
     }
     
     func reverse(text: String) -> String {
